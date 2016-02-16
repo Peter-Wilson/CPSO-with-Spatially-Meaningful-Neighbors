c0af85872f5cbdbdbf150954186e782243620b07
Peter Wilson <pw12nb@brocku.ca>
2016-02-16 15:54:30 -0500

    Added tests for the CPSO classes

--- src/cpso/CPSO.java
+++ src/cpso/CPSO.java
@@ -5,17 +5,70 @@
  */
 package cpso;
 
+import cpso.Classes.Particle;
+import cpso.Classes.Swarm;
+
 /**
  *
  * @author Peter
  */
-public class CPSO {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
+public class CPSO {    
     
+        int loops;
+        boolean problemSatisfied = false;
+        boolean min = true;
+        Swarm[] swarms; 
+        
+        //calculate the fitness of the PSO
+        public void PSO()
+        {
+            int loopsSinceNoNewGlobalBest = 0;
+            while (!problemSatisfied)
+            {
+                for (Swarm s : swarms)
+                {
+                    for(Particle i : s.getParticles()){
+                        double fitness = CalculateFitness(i.getPosition());
+                        if (i.UpdatePersonalBest(fitness, i.getPosition(), min))
+                            writeOutput("New Particle " + i + " best: x=" + i.getPosition());
+                        if ((s.getGlobalBest() == null) ||
+                            (i.getFitness() < s.getGlobalBest().getFitness() && min) ||
+                            (s.getGlobalBest().getFitness() < i.getFitness() && !min))
+                        {
+
+                            s.setGlobalBest(i);
+                            loopsSinceNoNewGlobalBest = 0;
+                            writeOutput("New Particle " + i + " best: x=" + i.getPosition());
+                        }
+                    }
+                    
+                    for (Particle i : s.getParticles())
+                    {
+                        s.UpdateVelocity(i);
+                        s.UpdatePosition(i);
+                    }
+                }
+                
+
+                if (++loopsSinceNoNewGlobalBest == 5) 
+                    problemSatisfied = true;
+            }
+           // writeOutput("Final Best X Value is...."+s..getPosition());
+        }
+
+        /**
+         * Calculate the fitness of the current swarm
+         * @param position the current position
+         * @return 
+         */
+        public double CalculateFitness(double position)
+        {
+            return (3 - Math.pow(position, 2));
+                
+        }
+
+        private void writeOutput(String output)
+        {
+            System.out.println(output);
+        }
 }
