621fa194e4e96936e2c7b7348d01c0dfcb1156b8
unknown <pw12nb@COSC-J31013.msad.cosc.brocku.ca>
2016-04-13 23:56:20 -0400

    added the workings for multi dimensional delaunay

--- src/triangulation/DulaunayTriangulation.java
+++ src/triangulation/DulaunayTriangulation.java
@@ -18,32 +18,11 @@ public class DulaunayTriangulation {
         int numParticles = particles.length;
         int[][] adjMatrix = new int[numParticles][numParticles];
         
-        // determine if i-j-k is a circle with no interior points 
-        for (int i = 0; i < numParticles; i++) {
-            for (int j = i+1; j < numParticles; j++) {
-                for (int k = j+1; k < numParticles; k++) {
-                    boolean isTriangle = true;
-                    //confirms no other points are inside the triangle
-                    for (int a = 0; a < numParticles; a++) {
-                        if (a == i || a == j || a == k) continue;
-                        if (particles[a].inside(particles[i], particles[j], particles[k])) {
-                           isTriangle = false;
-                           break;
-                        }
-                    }
-                    //if no, then this is the right triangulation
-                    //set up adjacency matrix connections
-                    if (isTriangle) {
-                        adjMatrix[i][j] = 1;                        
-                        adjMatrix[i][k] = 1;
-                        adjMatrix[j][i] = 1;                        
-                        adjMatrix[j][k] = 1;
-                        adjMatrix[k][i] = 1;                        
-                        adjMatrix[k][j] = 1;
-                    }
-                }
-            }
-        } 
+        //convert to dimension+1
+        //the extra coordinate = |p|^2 (where p = point)
+        //calculate convex hull on these points
+               //quick hull works with any dimensionality
+        //remove last dimension from convex hull
         
         return adjMatrix;
     }
@@ -51,7 +30,10 @@ public class DulaunayTriangulation {
     public static Particle chooseBestNeighbour(Particle[] particles, int index, int[] adjMatrix)
     {
         boolean hasConnectedNeighbours = false;
+        Particle best = null;
         
+        //choose best from list (see algorithm)
         
+        return best;        
     }
 }
