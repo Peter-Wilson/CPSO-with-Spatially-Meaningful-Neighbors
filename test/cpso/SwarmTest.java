421f31b9124ba4f97f8aa38ed12de811059155ab
unknown <pw12nb@COSC-J31013.msad.cosc.brocku.ca>
2016-04-25 18:57:06 -0400

    fixed the tests, new tests added for all new functions

--- test/cpso/SwarmTest.java
+++ test/cpso/SwarmTest.java
@@ -5,6 +5,8 @@
  */
 package cpso;
 
+import java.util.Random;
+import org.hamcrest.core.Is;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -56,9 +58,51 @@ public class SwarmTest {
     @Test
     public void testGetRandomNumber() {
         System.out.println("testGetRandomNumber");
-        Swarm instance = null;
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        double random = 0;
+        double expected = 0;
+        Random rand = new Random();
+        
+        //log function
+        expected = 50;
+        Swarm instance = new Swarm(20, 0.2, 0.3, 0.5, true, 1, 0);
+        random = instance.getRandomNumber(rand, instance.function);
+        assertTrue(random >= -expected);
+        assertTrue(random <= expected);
+        
+        //schaffer function
+        expected = 100;
+        instance = new Swarm(20, 0.2, 0.3, 0.5, true, 1, 1);
+        random = instance.getRandomNumber(rand, instance.function);
+        assertTrue(random >= -expected);
+        assertTrue(random <= expected);
+        
+        //Rastrigin function
+        expected = 5.12;
+        instance = new Swarm(20, 0.2, 0.3, 0.5, true, 1, 2);
+        random = instance.getRandomNumber(rand, instance.function);
+        assertTrue(random >= -expected);
+        assertTrue(random <= expected);
+        
+        //Rosenbrock function
+        expected = 30;
+        instance = new Swarm(20, 0.2, 0.3, 0.5, true, 1, 3);
+        random = instance.getRandomNumber(rand, instance.function);
+        assertTrue(random >= -expected);
+        assertTrue(random <= expected);
+        
+        //Griewanck function
+        expected = 600;
+        instance = new Swarm(20, 0.2, 0.3, 0.5, true, 1, 4);
+        random = instance.getRandomNumber(rand, instance.function);
+        assertTrue(random >= -expected);
+        assertTrue(random <= expected);
+        
+        //Ackley function
+        expected = 32;
+        instance = new Swarm(20, 0.2, 0.3, 0.5, true, 1, 5);
+        random = instance.getRandomNumber(rand, instance.function);
+        assertTrue(random >= -expected);
+        assertTrue(random <= expected);
     }
     
     /**
@@ -123,18 +167,6 @@ public class SwarmTest {
         {
             assertNotNull(result[i]);
         }
-    }
-
-    /**
-     * Test of CalculateDelaunatTriangulation3D method, of class Swarm.
-     */
-    @Test
-    public void testCalculateDelaunatTriangulation3D() throws Exception {
-        System.out.println("CalculateDelaunatTriangulation3D");
-        Swarm instance = null;
-        instance.CalculateDelaunatTriangulation3D();
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
     }
     
 }
